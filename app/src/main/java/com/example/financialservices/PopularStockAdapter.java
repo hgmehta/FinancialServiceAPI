b8e5f0174fc4c4eaaa8a62ce87f29124782d1f41
Harsh Mehta <harsh_m@hotmail.com>
2020-02-07 12:52:13 -0500

    Added ignore file

---
+++ app/src/main/java/com/example/financialservices/PopularStockAdapter.java
@@ -0,0 +1,43 @@
+package com.example.financialservices;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class PopularStockAdapter extends RecyclerView.Adapter<PopularStockAdapter.ViewHolder> {
+
+    private View.OnClickListener listener;
+    private Context context;
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder
+    {
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            itemView.setTag(this);
+            itemView.setOnClickListener(listener);
+
+
+
+        }
+    }
+}
