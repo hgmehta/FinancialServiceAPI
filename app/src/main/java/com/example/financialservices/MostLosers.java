116f45039449ee7e8e46ff01709de7489bca2734
Himanshu <himanshushah96@gmail.com>
2020-02-07 16:55:09 -0500

    API call  in most losers.java

--- app/src/main/java/com/example/financialservices/MostLosers.java
+++ app/src/main/java/com/example/financialservices/MostLosers.java
@@ -4,16 +4,72 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import java.util.ArrayList;
 
 
 public class MostLosers extends Fragment {
 
+    PopularStockAdapter popularStockAdapter;
+    ArrayList<MostLoserStock> mostLoserStocks;
+    View layoutView;
+
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        layoutView=view;
+
+        iDataService service = RetrofitClient.getRetrofitInstance().create(iDataService.class);
+
+        Call<MostLoser> call = service.getMostLoser();
+
+        call.enqueue(new Callback<MostLoser>() {
+            @Override
+            public void onResponse(Call<MostLoser> call, Response<MostLoser> response) {
+
+                MostLoser mostLoser=response.body();
+
+                try {
+                    mostLoserStocks=new ArrayList<>(mostLoser.getMostLoserStock());
+                    generateRecyclerView(layoutView,mostLoserStocks);
+                }catch (NullPointerException exception){
+
+                }
+
+            }
+
+            @Override
+            public void onFailure(Call<MostLoser> call, Throwable t) {
+                Toast.makeText(getActivity().getApplicationContext(), "Something Went Wrong!" + t.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+
+    public void generateRecyclerView(View view,ArrayList<MostLoserStock> mostLoserStocks){
+        popularStockAdapter = new PopularStockAdapter(mostLoserStocks, getActivity().getApplicationContext(),0);
+
+        LinearLayoutManager manager = new LinearLayoutManager(getActivity().getApplicationContext(), LinearLayoutManager.VERTICAL, false);
+
+        RecyclerView recyclerView = view.findViewById(R.id.recycle_most_gainers);
+
+        recyclerView.setLayoutManager(manager);
+        recyclerView.setAdapter(popularStockAdapter);
+    }
 
     public MostLosers() {
         // Required empty public constructor
